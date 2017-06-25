e61fd17a8315b63f717a3c68cd23ac58427fb79d
buffalobuffalobuffalo <nicholas.banker@gmail.com>
2017-06-25 14:26:50 -0400

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,16 @@
-PLACE YOUR CODE HERE
+import csv
+
+with open('faculty.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    next(csv_reader)
+    
+    emaillist = []
+    for row in csv_reader:
+        emaillist.append(row[3])
+
+csvemailfile = 'emails.csv'
+
+with open(csvemailfile, 'w') as output:
+    writer = csv.writer(output, lineterminator='\n')
+    for val in emaillist:
+        writer.writerow([val])
