21fa9d87f32b31631333e55a87160f9576ac6e18
buffalobuffalobuffalo <nicholas.banker@gmail.com>
2017-06-25 09:57:26 -0400

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,21 @@
 # Hint:  use Google to find python function
 
 ####a) 
+
+from datetime import datetime
+
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
+
+print datetime(2013,01,02) - datetime(2015,07,28)
 
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+print datetime(2013,12,31) - datetime(2015,05,28)
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+print datetime(1994,01,15) - datetime(2015,07,14)
